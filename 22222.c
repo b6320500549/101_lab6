f24f62230855b9400bfe6446129b3783acc9a00e
b6320500549 <jirasin.po@ku.th>
2021-03-15 19:53:31 +0700

    Update 22222.c

--- 22222.c
+++ 22222.c
@@ -2,18 +2,22 @@
 int main()
 
 {
-   int n,i,j;
+   int n,i,j,p=0,k=0;
    scanf("%d",&n);
    if(n>=4&&n<=9)
    {
      char m[2][n];
    for(i=0;i<2;i++)
    {
-       for(j=0;j<n;j++)
-   {
        scanf(" %[^\n]s",m[i]);
    }
+   for(j=0;j<n;j++)
+   {
+       if(m[0][j]== m[1][j])
+         p++;
+       else k++;
    }
+   printf("%d %d",p,k);
 
    }
 }
