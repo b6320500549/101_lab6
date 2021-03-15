535f692108cbb64975c6883cc24b63424b453aff
b6320500549 <jirasin.po@ku.th>
2021-03-15 17:19:14 +0700

    Create 11111.c

---
+++ 11111.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+
+{
+   int n,i,j;
+   scanf("%d",&n);
+   if(n>=1&&n<=10000)
+   {
+      char m[n][1000];
+   for(i=0;i<n;i++)
+   {
+       scanf(" %[^\n]s",m[i]);
+   }
+   }
+
+
+
+
+}
