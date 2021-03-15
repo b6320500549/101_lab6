79494d953ac1add8fd411c1ce70efc70ead31125
b6320500549 <jirasin.po@ku.th>
2021-03-15 19:32:20 +0700

    Update 11111.c

--- 11111.c
+++ 11111.c
@@ -1,28 +1,36 @@
 #include<stdio.h>
+#include<string.h>
 int main()
-
 {
-   int n,i,j;
+   int n,i,j,k=0;
    scanf("%d",&n);
    if(n>=1&&n<=10000)
    {
-      char m[n][1000];
+      char m[n][1000],g[n][1000];
    for(i=0;i<n;i++)
-   {
        scanf(" %[^\n]s",m[i]);
-   }
-   for(j=0;j<26;j++)
-   {
-   for(i=0;i<n;i++)
-   {
+   for(i=1;i<n;i++)
+        for(j=0;j<i;j++)
         {
-            if((m[i][0]==65+j)||(m[i][0]==97+j))
-                printf("%s\n",m[i]);
+        for(k=0;k<1000;)
+        {
+           if(m[i][k] < m[j][k])
+          {
+              strcpy(g,m[j]);
+              strcpy(m[j],m[i]);
+              strcpy(m[i],g);
+              break;
+          }
+          else if(m[i][k] == m[j][k])
+          {
+              k++;
+          }
+          else break;
         }
-   }
+        }
 
-   }
+  for(i=0;i<n;i++)
+        printf("%s\n",m[i]);
 
-   }
-
+}
 }
